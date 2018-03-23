ee54057421e1bbf5361483e4f3306ce41aca3f0c
Marc <mgbuddy@gmail.com>
2018-03-23 18:52:52 +0000

    Added integration test

---
+++ tests/test_integracion.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Test de integración. Muy sencillo, por ahora.
+sleep 10
+echo "Test de integración"
+curl --silent "http://localhost/MainServlet" | grep "This is a skeleton application"
+RESULTAT=$?
+[ $RESULTAT -eq 0 ] && echo "TODO BIEN" || echo "ALGO HA FALLADO"
+exit $RESULTAT
