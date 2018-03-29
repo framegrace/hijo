9e89611df205ed9a054e788d41f747bf63e7f486
Marc <mgbuddy@gmail.com>
2018-03-29 17:36:20 +0000

    Testing failures

--- tests/test_integracion.sh
+++ tests/test_integracion.sh
@@ -2,7 +2,7 @@
 # Test de integración. Muy sencillo, por ahora.
 sleep 10
 echo "Test de integración"
-curl --silent "http://localhost/MainServlet" | grep "This is a skeleton application"
+curl --silent "http://localhost/MainServlet" | grep "This is a skreleton application"
 RESULTAT=$?
 [ $RESULTAT -eq 0 ] && echo "TODO BIEN" || echo "ALGO HA FALLADO"
 exit $RESULTAT
