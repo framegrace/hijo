d0ffeb58b6904ea8d344ab89aa534a6e79b5df32
Marc <mgbuddy@gmail.com>
2018-04-04 13:09:57 +0000

    Move from jtl to csv

--- tests/stress-test.sh
+++ tests/stress-test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "### Running JMeter performance test ###"
 # Clear out old results
-rm $WORKSPACE/jmeter.jtl
+rm $WORKSPACE/jmeter.csv > /dev/null 2>&1 || true
 # Run the tests
 echo "## Running the tests"
 cd "$WORKSPACE"
-jmeter -n -t tests/jmeter.jmx -l $WORKSPACE/jmeter.jtl
+jmeter -n -t tests/jmeter.jmx -l $WORKSPACE/jmeter.csv
